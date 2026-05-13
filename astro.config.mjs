c3d0d71c57f2c29ca0d683442499e580c7e4bdca
angelypl <angelpayano1503@gmail.com>
2026-05-13 19:35:18 +0000

    Switch to static deployment to fix 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  server: {
-    host: true,
-    port: 3000
-  }
+  output: 'static'
 });
